80f4a0ad6e544e09339bc116bb477c59bc85da2d
TessieWaithira <terrywaithira1@gmail.com>
2017-11-07 22:42:36 +0300

    set media_url and media_route in settings.py

--- weeklylist/models.py
+++ weeklylist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class List(models.Model):
+	tagline = models.CharField(max_length=255)
+	friend = models.ForeignKey(User, related_name='lists')
+	subject = models.CharField(max_length=255)
+	signoff = models.TextField(max_length=4000)
+	created_at = models.DateTimeField(auto_now_add=True)
